1566c25e090898f53c9b28d32921a3cda324766a
Nick Simpson <35578656+simpsonnl@users.noreply.github.com>
2020-02-13 19:57:31 -0500

    Update spotify_token_swap.rb

--- spotify_token_swap.rb
+++ spotify_token_swap.rb
@@ -37,9 +37,9 @@ require 'encrypted_strings'
 #
 # Find your app at https://developer.spotify.com/my-applications/#!/applications
 
-CLIENT_ID = "e6695c6d22214e0f832006889566df9c" # TODO: Get it from Spotify
-CLIENT_SECRET = "29eb02041ba646179a1189dccac112c7" # TODO: Get it from Spotify
-CLIENT_CALLBACK_URL = "spotifyiossdkexample://" # TODO: Get it from Spotify
+CLIENT_ID = "52c0f5ab6e5f4a2f83da6c5fad1c6bac" # TODO: Get it from Spotify
+CLIENT_SECRET = "a66d0d708a1f49789372f50a65d2b3cc" # TODO: Get it from Spotify
+CLIENT_CALLBACK_URL = "https://localhost:44385/Authorize/Callback/" # TODO: Get it from Spotify
 
 # I don't think the secret should be changed...
 ENCRYPTION_SECRET = "cFJLyifeUJUBFWdHzVbykfDmPHtLKLGzViHW9aHGmyTLD8hGXC"
